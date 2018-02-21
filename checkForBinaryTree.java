2cfa22190459e2f7502fbb175740982451dfcbf2
ap8ff <ap8ff@mail.umsl.edu>
2018-02-21 12:00:19 -0600

    testing the user

--- checkForBinaryTree.java
+++ checkForBinaryTree.java
@@ -1,4 +1,5 @@
 
+
 /* Hidden stub code will pass a root argument to the function below. Complete the function to solve the challenge. Hint: you may want to write one or more helper functions.
 
 The Node class is defined as follows:
