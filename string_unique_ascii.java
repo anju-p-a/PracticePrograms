c943ddf9d539a62cad1ddad741d39efb6e437da8
anju-p-a <ap8ff@mail.umsl.edu>
2018-04-16 20:01:49 -0500

    Create string_unique_ascii.java
    checks if a given string has unique characters

---
+++ string_unique_ascii.java
@@ -0,0 +1,19 @@
+public class MyClass {
+    public static void main(String args[]) {
+       String input = "Heloasdf";
+       boolean [] arrayIndicator = new boolean[256]; // this is an array which will be set to true for  characters iterated through the string
+       for(int i = 0;i<input.length();i++){
+           int index = input.charAt(i);
+            if(arrayIndicator[index]== false){  
+                arrayIndicator[index] = true;
+            }else {                                 // for a given character in the loop if the array indicator is already true , then the string is not unique
+                System.out.println("Contains non unique characters");
+                return;
+                
+            }
+          
+       }
+        System.out.println("The given string is unique");
+       
+    }
+}
